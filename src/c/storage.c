b4ddad2fe048edd2b540ef45b88f1964776a936f
Sheldon Smith <sheldon@autointel.io>
2026-04-05 09:08:26 -0400

    feature: exit time stored so app resumes when re-launched as if it was running in the background

--- src/c/storage.c
+++ src/c/storage.c
@@ -8,6 +8,7 @@
 #define PERSIST_KEY_GAME_STATE    3
 #define PERSIST_KEY_PLAY_RUNNING  4
 #define PERSIST_KEY_PLAY_SECONDS  5
+#define PERSIST_KEY_EXIT_TIME     6
 
 #define GAME_CLOCK_DEFAULT (25 * 60)
 #define PLAY_CLOCK_START   25
@@ -19,8 +20,23 @@ void storage_load(void) {
   game_clock_set_default_seconds(game_default);
 
   if (persist_exists(PERSIST_KEY_GAME_CURRENT)) {
-    game_clock_set_total_seconds(persist_read_int(PERSIST_KEY_GAME_CURRENT));
-    game_clock_set_state(persist_read_int(PERSIST_KEY_GAME_STATE));
+    int stored_seconds = persist_read_int(PERSIST_KEY_GAME_CURRENT);
+    int game_state     = persist_read_int(PERSIST_KEY_GAME_STATE);
+
+    if (game_state == 2 && persist_exists(PERSIST_KEY_EXIT_TIME)) {
+      int elapsed   = (int)time(NULL) - persist_read_int(PERSIST_KEY_EXIT_TIME);
+      int remaining = stored_seconds - elapsed;
+      if (remaining <= 0) {
+        game_clock_set_total_seconds(game_default);
+        game_clock_set_state(0);
+      } else {
+        game_clock_set_total_seconds(remaining);
+        game_clock_set_state(2);
+      }
+    } else {
+      game_clock_set_total_seconds(stored_seconds);
+      game_clock_set_state(game_state);
+    }
   } else {
     game_clock_set_total_seconds(game_default);
     game_clock_set_state(0);
@@ -43,6 +59,12 @@ void storage_save(void) {
   if (game_clock_ever_started() && !game_clock_is_running()) game_state = 1;
   if (game_clock_is_running())                               game_state = 2;
   persist_write_int(PERSIST_KEY_GAME_STATE, game_state);
+
+  if (game_state == 2) {
+    persist_write_int(PERSIST_KEY_EXIT_TIME, (int)time(NULL));
+  } else {
+    persist_delete(PERSIST_KEY_EXIT_TIME);
+  }
 
   persist_write_int(PERSIST_KEY_PLAY_RUNNING, play_clock_is_running() ? 1 : 0);
   persist_write_int(PERSIST_KEY_PLAY_SECONDS, play_clock_get_seconds());
